5a7ff716413b71b2e25e46a39e2b806fa4cec08e
Veronicah Wanjuu <veronicah.wanjuu@example.com>
2025-07-21 19:34:43 -0400

    Configure for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/VERONICAH-S-SDR-RESUME/",
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
